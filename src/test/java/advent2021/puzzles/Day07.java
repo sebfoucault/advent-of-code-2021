56c7a304c30d880aab00e2b826ce533d94db2d19
sbfclt <sebastien.foucault@gmail.com>
2021-12-07 21:28:53 +0100

    Add day 7

---
+++ src/test/java/advent2021/puzzles/Day07.java
@@ -0,0 +1,81 @@
+package advent2021.puzzles;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.jupiter.api.Test;
+
+import advent2021.misc.Utils;
+
+public class Day07 {
+
+   
+   @Test
+   void part1() throws IOException {
+      
+      List<Integer> positions = Utils.readSingleValueFromResources("/day07.txt",
+            s -> Utils.split(s, ",", Integer::parseInt));
+      
+      Result r = align(positions, (from, to) -> Math.abs(to - from));
+        
+      System.out.println(r.position + " => " + r.cost);
+   }
+
+   @Test
+   void part2() throws IOException {
+      
+      List<Integer> positions = Utils.readSingleValueFromResources("/day07.txt",
+            s -> Utils.split(s, ",", Integer::parseInt));
+      
+      Result r = align(positions, (from, to) -> {
+         var distance = Math.abs(to - from);
+         return distance * (distance + 1) / 2;
+      });
+        
+      System.out.println(r.position + " => " + r.cost);
+   }
+   
+   class Result {
+      int position;
+      int cost;
+   }
+   
+   interface CostFunction {
+      int computeCostFromTo(int from, int to);
+   }
+   
+   private Result align(List<Integer> values, CostFunction costFunction) {
+
+      Map<Integer, Integer> valuesOccurrences = new HashMap<>();
+      
+      int minValue = Integer.MAX_VALUE;
+      int maxValue = 0;
+      for (int value: values) {
+         valuesOccurrences.merge(value, 1, (v1, v2) -> v1 + v2);
+         minValue = Math.min(minValue, value);
+         maxValue = Math.max(maxValue, value);
+      }
+      
+      Result result = null;
+      
+      for (int position = minValue; position <= maxValue; position++) {
+         
+         int costToPosition = 0;
+         
+         for (Entry<Integer, Integer> e: valuesOccurrences.entrySet()) {
+            costToPosition += costFunction.computeCostFromTo(e.getKey(), position) * e.getValue();
+         }
+         
+         if (result == null || costToPosition < result.cost) {
+            result = new Result();
+            result.cost = costToPosition;
+            result.position = position;
+         }
+      }
+      
+      return result;
+   }
+}
