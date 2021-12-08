35651dae91e0ffbe5e34d3f5f47a584cc4fc1a13
sbfclt <sebastien.foucault@gmail.com>
2021-12-08 22:09:11 +0100

    Add day 8

---
+++ src/test/java/advent2021/puzzles/Day08.java
@@ -0,0 +1,150 @@
+package advent2021.puzzles;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import advent2021.misc.Utils;
+
+public class Day08 {
+
+   static class Sample {
+
+      final List<Set<Character>> patterns;
+      final List<Set<Character>> outputs;
+
+      Sample(List<Set<Character>> patterns, List<Set<Character>> outputs) {
+         this.patterns = patterns;
+         this.outputs = outputs;
+      }
+      
+      static Sample parse(String s) {
+
+         List<String> splits = Utils.split(s, "|");
+
+         String patternsAsString = splits.get(0);
+         String outputsAsString = splits.get(1);
+
+         List<Set<Character>> patterns = Utils.split(patternsAsString, " ").stream().map(Sample::toCharacterSet)
+               .collect(Collectors.toList());
+
+         List<Set<Character>> outputs = Utils.split(outputsAsString, " ").stream().map(Sample::toCharacterSet)
+               .collect(Collectors.toList());
+
+         return new Sample(patterns, outputs);
+      }
+      
+      private static Set<Character> toCharacterSet(String s) {
+
+         Set<Character> result = new TreeSet<>();
+         for (int i = 0; i < s.length(); i++) {
+            result.add(s.charAt(i));
+         }
+         return result;
+      }
+   }
+
+   @Test
+   void part1() throws IOException {
+
+      List<Sample> samples = Utils.readValuesFromResources("/day08.txt", Sample::parse);
+
+      int matching = 0;
+      for (Sample sample : samples) {
+         for (Set<Character> output : sample.outputs) {
+            int size = output.size();
+            if (size == 2 || size == 3 || size == 4 || size == 7) {
+               matching++;
+            }
+         }
+      }
+
+      System.out.println(matching);
+   }
+
+   @Test
+   void part2() throws IOException {
+
+      List<Sample> samples = Utils.readValuesFromResources("/day08.txt", Sample::parse);
+
+      int sum = samples.stream()
+            .map(s -> Solver.apply(s, Solver.solve(s)))
+            .reduce(0, Integer::sum);
+      
+      System.out.println(sum);
+   }
+
+   static class Solver {
+
+      static Map<Set<Character>, Integer> solve(Sample s) {
+
+         Map<Set<Character>, Integer> result = new LinkedHashMap<>();
+
+         List<Set<Character>> candidates = new ArrayList<>(s.patterns);
+
+         Set<Character> one = findAndRemove(candidates, hasSize(2));
+         Set<Character> four = findAndRemove(candidates, hasSize(4));
+         Set<Character> seven = findAndRemove(candidates, hasSize(3));
+         Set<Character> eight = findAndRemove(candidates, hasSize(7));
+         Set<Character> six = findAndRemove(candidates, hasSize(6).and(hasIntersectionOfSize(one, 1)));
+         Set<Character> nine = findAndRemove(candidates, hasSize(6).and(hasIntersectionOfSize(four, four.size())));
+         Set<Character> zero = findAndRemove(candidates, hasSize(6));
+         Set<Character> three = findAndRemove(candidates, hasIntersectionOfSize(one, one.size()));
+         Set<Character> five = findAndRemove(candidates, hasIntersectionOfSize(nine, 5));
+         Set<Character> two = findAndRemove(candidates, set -> true);
+
+         result.put(zero, 0);
+         result.put(one, 1);
+         result.put(two, 2);
+         result.put(three, 3);
+         result.put(four, 4);
+         result.put(five, 5);
+         result.put(six, 6);
+         result.put(seven, 7);
+         result.put(eight, 8);
+         result.put(nine, 9);
+
+         return result;
+      }
+
+      private static Predicate<Set<Character>> hasIntersectionOfSize(Set<Character> refSet, int intersectionSize) {
+         return set -> intersect(set, refSet).size() == intersectionSize;
+      }
+
+      private static <T> Set<T> intersect(Set<T> s1, Set<T> s2) {
+         Set<T> s = new HashSet<>(s1);
+         s.retainAll(s2);
+         return s;
+      }
+
+      static Predicate<Set<Character>> hasSize(int size) {
+         return set -> set.size() == size;
+      }
+
+      static Set<Character> findAndRemove(List<Set<Character>> candidates, Predicate<Set<Character>> predicate) {
+
+         Set<Character> result = candidates.stream().filter(predicate).findFirst().orElseThrow();
+         candidates.remove(result);
+         return result;
+      }
+
+      static int apply(Sample sample, Map<Set<Character>, Integer> map) {
+
+         int result = 0;
+         for (Set<Character> output : sample.outputs) {
+            result *= 10;
+            result += map.get(output);
+         }
+         return result;
+      }
+   }
+}
